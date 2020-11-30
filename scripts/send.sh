382edbd5cad26b1ad4ccf85e028d6351a379214e
KP Singh <kpsingh@google.com>
2020-11-30 22:43:13 +0000

    save

--- scripts/send.sh
+++ scripts/send.sh
@@ -1,5 +1,4 @@
 git send-email \
-	--cc-cmd "~/recipients" \
+	--cc-cmd "./recipients" \
 	--to linux-kernel@vger.kernel.org \
-	--to bpf@vger.kernel.org \
-	~/patches/krsi_test_fix/PATCH_bpf/v1
+	~/patches/might_fault/PATCH/v1

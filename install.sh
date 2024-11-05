4bd22ac7268117710a6d0a33396c5b3c1fc82e49
KP Singh <kpsingh@kernel.org>
2024-11-05 23:29:37 +0100

    fix vscode configs

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 GITCONFIG="${HOME}/.gitconfig"
+VIMRC="${HOME}/.vimrc"
 BASHRC="${HOME}/.bashrc"
+VSCODE_SETTINGS="${HOME}/.vscode/settings.json"
 
 set -ex
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
@@ -14,4 +16,21 @@ fi
 
 if [[ ! -f "${BASHRC}" ]]; then
     ln -s "${DIR:?}/bashrc" "${BASHRC:?}"
+fi
+
+if [[ ! -f "${VIMRC}" && ! -d "${HOME}/.vim" ]]; then
+    ln -s "${DIR:?}/vimrc" "${VIMRC:?}"
+    git clone https://github.com/VundleVim/Vundle.vim.git ${HOME}/.vim/bundle/Vundle.vim
+    vim +PluginInstall +qall
+fi
+
+if [[ ! -f "${VIMRC}" && ! -d "${HOME}/.vim" ]]; then
+    ln -s "${DIR:?}/vimrc" "${VIMRC:?}"
+    git clone https://github.com/VundleVim/Vundle.vim.git ${HOME}/.vim/bundle/Vundle.vim
+    vim +PluginInstall +qall
+fi
+
+if [[ ! -f "${VSCODE_SETTINGS}" ]]; then
+    mkdir -p ${HOME}/.vscode
+    ln -s "${DIR:?}/vscode-settings.json" "${VSCODE_SETTINGS:?}"
 fi
--- vscode/README.md
+++
@@ -1,8 +0,0 @@
-```
-wget https://raw.githubusercontent.com/llvm/llvm-project/master/lldb/examples/python/x86_64_linux_target_definition.py -O .vscode/x86_64_linux_target_definition.py
-```
-
-```
-cp -r kernel-vscode ${KERNEL_CHECKOUT:?}/.vscode
-```
-
